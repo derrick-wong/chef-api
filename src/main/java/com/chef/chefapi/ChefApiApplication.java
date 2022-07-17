4ea2950d19a23428d6075f72ea8e55d567f4e182
Derrick Wong <derrick.c.wong@gmail.com>
2022-07-16 20:32:32 -0700

    first commit

---
+++ src/main/java/com/chef/chefapi/ChefApiApplication.java
@@ -0,0 +1,13 @@
+package com.chef.chefapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chef/chefapi/ChefApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chef.chefapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
